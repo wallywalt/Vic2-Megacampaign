17534f5389b04026263f2c6b4b8e7431599681a2
JustinHMKim <35758501+JustinHMKim@users.noreply.github.com>
2019-06-17 21:39:05 -0700

    Changed the loading screen images again, added New World colonial war options, changed some NA borders for aesthetics

--- MegaCampaign/history/provinces/usa/correct_coresKOJ.py
+++ MegaCampaign/history/provinces/usa/correct_coresKOJ.py
@@ -3,4 +3,4 @@ import glob
 import sys
 
 for line in fileinput.input(glob.glob('*.txt'), inplace=True):
-    sys.stdout.write(line.replace('controller=NEN', 'controller=NEN\nadd_core=ENG'))
+    sys.stdout.write(line.replace('controller=GEO/nadd_core=GEO', 'controller=GEO\nadd_core=GEO'))
