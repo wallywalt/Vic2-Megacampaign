eeba25be2668a5b4f8b4d7b247fac87b5f70b2bf
JustinHMKim <35758501+JustinHMKim@users.noreply.github.com>
2019-09-28 21:55:10 -0700

    Changed all countries to open borders and unitary to fix initial immigration, made all of NA states to avoid awkward immigration (will make the interior colonial again to concentrate immigration into the coastlines, added some more events for Ming and its substates regarding gain and loss of provinces, removed music from the main mod to reduce base size, changed localisation for communists and anarcho-liberals (plus adjusted anarcho-liberal politics), increased chance of nationalist rebels vis-a-vis other rebel types, added France-Kale events to make Kale an active issue for France, changed Korean flag and name

--- MegaCampaign/history/countries/correct_coresKOJ.py
+++ MegaCampaign/history/countries/correct_coresKOJ.py
@@ -3,4 +3,4 @@ import glob
 import sys
 
 for line in fileinput.input(glob.glob('*.txt'), inplace=True):
-    sys.stdout.write(line.replace('add_core=ENG', ''))
+    sys.stdout.write(line.replace('ruling_party', '#New Reforms\nconscription = two_year_draft\ndebt_law = serfdom\npenal_system = capital_punishment\nchild_labor = child_labor_legal\ncitizens_rights = primary_culture_only\nborder_policy = open_borders\ncentralization = unitary\ncolonial_policy = no_colonies\n\nruling_party'))
--- MegaCampaign/history/pops/1836.1.1/correct_coresKOJ.py
+++
@@ -1,6 +0,0 @@
-import fileinput
-import glob
-import sys
-
-for line in fileinput.input(glob.glob('*.txt'), inplace=True):
-    sys.stdout.write(line.replace('japanese', 'nihonjin'))
